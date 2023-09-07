6ae278b55ceb8a78f363cb4f4f83acb86b7575b3
mohammedbabekir <mohamed.babekir.ali@gmail.com>
2023-09-07 10:56:40 -0700

    aa

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,13 +3,13 @@
 /**
 *main - Entry point
 *
-*Description: A C program that prints with put function
+*Description: A C program that prints with put function.
 *
 *Return: Always 0 (success)
 */
 
 int main(void)
 {
-        printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
