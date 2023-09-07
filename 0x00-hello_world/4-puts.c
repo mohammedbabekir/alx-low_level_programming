fe84a0a6ae4589c3c1a1ca29da65c2a7aba1bf39
mohammedbabekir <mohamed.babekir.ali@gmail.com>
2023-09-07 10:21:33 -0700

    aa

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,9 @@
 *
 *Description: A C program that prints with put function
 *
-*return: Always 0 (success)
+*Return: Always 0 (success)
 */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
