99a7e984fb8a45d5c7a971ab65358a6e6900b713
mohammedbabekir <mohamed.babekir.ali@gmail.com>
2023-09-08 13:39:21 -0700

    m

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,13 +11,12 @@ int main(void)
 {
 	int ones;
 	int tens;
-	for (tens = '0'; tens <= '9'; tens++) /*increment tens*/
+	for (tens = '0'; tens <= '9'; tens++)
 	{
 		for (ones = (tens + 1); ones <= '9'; ones++) /*one's ten+1*/
 		{
 			putchar(tens);
 			putchar(ones);
-
 			if (tens != '8' || ones != '9') /*print commas*/
 			{
 				putchar(',');
