d4564e5889a923e991c5b85d96fc18f7abf40497
mohammedbabekir <mohamed.babekir.ali@gmail.com>
2023-09-08 13:33:16 -0700

    m

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,22 +10,20 @@
 int main(void)
 {
 	int n;
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
-	if (n > 0)
+	printf("Last digit of %i is %i and is ", n, (n % 10));
+	if ((n % 10) == 0)
 	{
-		printf("%i is positive\n", n);
+		printf("0\n");
 	}
-	else if (n == 0)
+	else if ((n % 10) > 5)
 	{
-		printf("%i is zero\n", n);
+		printf("greater than 5\n");
 	}
-	else if (n < 0)
+	else
 	{
-		printf("%i is negative\n", n);
+		printf("less than 6 and not 0\n");
 	}
-
 	return (0);
 }
--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  *main - print 00 to 99 with no duplicate digits or combos: no 11, no 10 (01)
@@ -10,7 +11,6 @@ int main(void)
 {
 	int ones;
 	int tens;
-
 	for (tens = '0'; tens <= '9'; tens++) /*increment tens*/
 	{
 		for (ones = (tens + 1); ones <= '9'; ones++) /*one's ten+1*/
@@ -26,6 +26,5 @@ int main(void)
 		}
 	}
 	putchar('\n');
-
 	return (0);
 }
