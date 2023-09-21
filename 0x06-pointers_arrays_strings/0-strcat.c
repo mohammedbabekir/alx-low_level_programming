dddc5300683c45c97966dd06b4c05dad12d2597e
mohammedbabekir <mohamed.babekir.ali@gmail.com>
2023-09-21 03:43:39 -0700

    done

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,8 +10,8 @@
 char *_strcat(char *dest, char *src)
 {
 	int i, j;
-	for (i = 0; dest[i]; i++)
-		;
+	for (i = 0; dest[i]; i++);
+
 	for (j = 0; src[j]; j++)
 		dest[i + j] = src[j];
 	dest[i + j] = '\0';
