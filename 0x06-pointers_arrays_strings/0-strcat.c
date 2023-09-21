d32af13414bef555eb92811bd72ffa9d29b72b07
mohammedbabekir <mohamed.babekir.ali@gmail.com>
2023-09-21 03:44:48 -0700

    done

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,13 +5,13 @@
  * @dest: destination string
  * @src: source string
  * Return: pointer to resulting string dest
-*/
-
+ */
 char *_strcat(char *dest, char *src)
 {
 	int i, j;
-	for (i = 0; dest[i]; i++);
 
+	for (i = 0; dest[i]; i++)
+		;
 	for (j = 0; src[j]; j++)
 		dest[i + j] = src[j];
 	dest[i + j] = '\0';
